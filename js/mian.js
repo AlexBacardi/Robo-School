73583e80b4efc575f7da9955675bc03c9ad46148
Alex <power19872006@yandex.ru>
2024-10-06 13:04:14 +0300

    create modal window

--- js/mian.js
+++ js/mian.js
@@ -33,12 +33,12 @@ $(document).ready(function () {
     let show = true;
     let countbox = ".about-item__title";
     $(window).on("scroll load resize", function () {
-        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
-        let w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
-        let e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
-        let w_height = $(window).height(); // Высота окна браузера
-        let d_height = $(document).height(); // Высота всего документа
-        let e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+        if (!show) return false; 
+        let w_top = $(window).scrollTop();
+        let e_top = $(countbox).offset().top;
+        let w_height = $(window).height();
+        let d_height = $(document).height();
+        let e_height = $(countbox).outerHeight();
         if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
             $('.num').css('opacity', '1');
             $('.num').spincrement({
@@ -48,6 +48,26 @@ $(document).ready(function () {
 
             show = false;
         }
+    });
+
+    //tabs
+    const tabBtns = document.querySelectorAll('.tabs .tabs__btn');
+
+    const tabsContent = document.querySelectorAll('.tabs__content .tabs__block');
+
+    tabBtns.forEach(function (elem) {
+        elem.addEventListener('click', function () {
+            tabBtns.forEach(function (elem) {
+                elem.classList.remove('tabs__btn--active');
+            });
+            tabsContent.forEach(function (item) {
+                item.classList.remove('tabs__block--active');
+            });
+            elem.classList.add('tabs__btn--active');
+            const tabIdSelector = this.getAttribute('data-tab');
+            const currentTabBlock = document.querySelector(tabIdSelector);
+            currentTabBlock.classList.add('tabs__block--active');
+        })
     });
 });
 
